e2a2449f8e5b7388173e836a312b72ea61c26ae7
AnggaaIs <servantangga@gmail.com>
2025-04-03 21:49:10 +0700

    fix: increase maximum length for Google auth callback parameters

--- src/models/validation/auth.validation.ts
+++ src/models/validation/auth.validation.ts
@@ -1,6 +1,6 @@
 import { Type } from "@sinclair/typebox";
 
 export const AuthGoogleCallbackBody = Type.Object({
-  code: Type.String({ minLength: 1, maxLength: 50 }),
-  state: Type.String({ minLength: 1, maxLength: 30 }),
+  code: Type.String({ minLength: 1, maxLength: 150 }),
+  state: Type.String({ minLength: 1, maxLength: 150 }),
 });
